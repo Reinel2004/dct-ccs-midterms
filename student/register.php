2ad453819e64a2cd51ac2241385b6835b1a4d39d
Reinel Gueco <venvengueco@gmail.com>
2024-11-13 02:39:27 +0800

    added attachment button

--- student/register.php
+++ student/register.php
@@ -95,6 +95,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
                         <td>
                             <a href="edit.php?student_id=<?php echo urlencode($student['student_id']); ?>" class="btn btn-info btn-sm">Edit</a>
                             <a href="delete.php?student_id=<?php echo urlencode($student['student_id']); ?>" class="btn btn-danger btn-sm">Delete</a>
+                            <a href="attach-subject.php?student_id=<?php echo urlencode($student['student_id']); ?>" class="btn btn-warning btn-sm">Attach Subject</a>
                         </td>
                     </tr>
                 <?php endforeach; ?>
