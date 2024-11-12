5c9acf8fda3e1cd0674da26dd90bdfdc9e8b265e
Reinel Gueco <venvengueco@gmail.com>
2024-11-13 00:22:05 +0800

    added some code inside functions

--- functions.php
+++ functions.php
@@ -1,29 +1,78 @@
 <?php
 
 function getUsers() {
-   
+    return [
+        ["email" => "user1@email.com", "password" => "password"],
+        ["email" => "user2@email.com", "password" => "password"],
+        ["email" => "user3@email.com", "password" => "password"],
+        ["email" => "user4@email.com", "password" => "password"],
+        ["email" => "user5@email.com", "password" => "password"]
+    ];
 }
 
 function validateLoginCredentials($email, $password) {
-  
+    $errors = [];
+
+    if (empty($email)) {
+        $errors[] = "Email is required.";
+    } elseif (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        $errors[] = "Invalid Email.";
+    } else {
+       
+        $users = getUsers();
+        $emailExists = false;
+        foreach ($users as $user) {
+            if ($user['email'] === $email) {
+                $emailExists = true;
+                break;
+            }
+        }
+
+        if (!$emailExists) {
+            $errors[] = "Invalid Email.";
+        }
+    }
+
+    if (empty($password)) {
+        $errors[] = "Password is required.";
+    }
+
+    return $errors;
+
 }
 
 
 function checkLoginCredentials($email, $password, $users) {
-    
+    foreach ($users as $user) {
+        if ($user['email'] === $email && $user['password'] === $password) {
+            return true;
+        }
+    }
+    return false;
 }
 
 function checkUserSessionIsActive() {
-   
+    if (isset($_SESSION['email']) && basename($_SERVER['PHP_SELF']) == 'index.php') {
+        header("Location: dashboard.php");
+        exit;
+    }
 }
 
 function guard() {
-    
+    if (empty($_SESSION['email']) && basename($_SERVER['PHP_SELF']) != 'index.php') {
+        header("Location: index.php"); 
+        exit;
+    }
 }
 
 
 function displayErrors($errors) {
-   
+    $output = "<ul>";
+    foreach ($errors as $error) {
+        $output . = "<li>" . htmlspecialchars($error) . "</li>";
+    }
+    $output . = "</ul>";
+    return $output;
 }
 
 
