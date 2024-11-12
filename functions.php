d0b539523ec2b4a3066a9329e337fcdc5b43372f
Reinel Gueco <venvengueco@gmail.com>
2024-11-13 00:13:17 +0800

    function names added

---
+++ functions.php
@@ -0,0 +1,80 @@
+<?php
+
+function getUsers() {
+   
+}
+
+function validateLoginCredentials($email, $password) {
+  
+}
+
+
+function checkLoginCredentials($email, $password, $users) {
+    
+}
+
+function checkUserSessionIsActive() {
+   
+}
+
+function guard() {
+    
+}
+
+
+function displayErrors($errors) {
+   
+}
+
+
+function renderErrorsToView($error) {
+   
+}
+
+
+function validateStudentData($student_data) {
+   
+}
+
+function checkDuplicateStudentData($student_data) {
+   
+}
+
+function getSelectedStudentIndex($student_id) {
+   
+}
+
+
+function getSelectedStudentData($index) {
+   
+}
+
+
+function getBaseURL() {
+   
+}
+
+
+function validateSubjectData($subject_data){
+  
+
+}
+
+function checkDuplicateSubjectData($subject_data){
+    
+}
+
+function getSelectedSubjectIndex($subject_code){
+
+}
+
+function getSelectedSubjectData($index){
+
+}
+
+function validateAttachedSubject($subject_data){
+
+}
+
+
+?>
