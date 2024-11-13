e0292bbcf74fdaae289f6056d3d34eed7574c701
Reinel Gueco <venvengueco@gmail.com>
2024-11-13 12:18:15 +0800

    add subject done

--- subject/add.php
+++ subject/add.php
@@ -3,7 +3,18 @@ session_start();
 $pageTitle = "Add Subject";
 include '../header.php';
 include '../functions.php';
-guard();
+
+if (empty($_SESSION['email'])) {
+    header("Location: ../index.php");
+    exit;
+}
+
+header("Cache-Control: no-store, no-cache, must-revalidate"); 
+header("Cache-Control: post-check=0, pre-check=0", false); 
+header("Pragma: no-cache");
+
+checkUserSessionIsActive();  
+guard(); 
 
 $errors = [];
 $subject_data = [];
